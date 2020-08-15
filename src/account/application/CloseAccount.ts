2b4d3e4148f89100849c042a275f6aa0f8cfbc80
Juan Abril <juandaabril@gmail.com>
2020-08-15 11:51:34 -0500

    add kata rich domain

--- src/account/application/CloseAccount.ts
+++ src/account/application/CloseAccount.ts
@@ -1,10 +1,6 @@
 import {AccountRepository} from "../domain/AccountRepository";
 import {AccountId} from "../domain/AccountId";
-import {DateService} from "../../shared/domain/DateService";
 import {CustomerId} from "../../customer/domain/CustomerId";
-import {Amount} from "../domain/Amount";
-import {TransactionDate} from "../domain/TransactionDate";
-import {Description} from "../domain/Description";
 
 export class CloseAccount {
     constructor(
