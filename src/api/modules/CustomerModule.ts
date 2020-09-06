c0d509892b49018d8d94ac164d89eb3e69db0b48
Juan Abril <juandaabril@gmail.com>
2020-09-06 07:28:30 -0500

    add acceptance tests

--- src/api/modules/CustomerModule.ts
+++ src/api/modules/CustomerModule.ts
@@ -1,17 +1,11 @@
 import {Global, Module} from "@nestjs/common";
-import {CloseAccount} from "../../core/account/application/CloseAccount";
-import {DepositFundsIntoAccount} from "../../core/account/application/DepositFundsIntoAccount";
-import {RegisterNewAccount} from "../../core/account/application/RegisterNewAccount";
-import {AccountRepository} from "../../core/account/domain/AccountRepository";
-import {DateService} from "../../core/shared/domain/DateService";
-import {FirebaseAccountRepository} from "../../core/account/infrastructure/FirebaseAccountRepository";
-import {InMemoryCustomerRepository} from "../../core/customer/infrastructure/InMemoryCustomerRepository";
 import {CreateCustomer} from "../../core/customer/application/CreateCustomer";
 import {CustomerRepository} from "../../core/customer/domain/CustomerRepository";
 import {GetCustomerDetails} from "../../core/customer/application/GetCustomerDetails";
+import {FirebaseCustomerRepository} from "../../core/customer/infrastructure/FirebaseCustomerRepository";
 
 //repositories
-const customerRepository = {provide: 'CustomerRepository', useClass: InMemoryCustomerRepository};
+const customerRepository = {provide: 'CustomerRepository', useClass: FirebaseCustomerRepository};
 
 //use cases
 const createCustomer = {
